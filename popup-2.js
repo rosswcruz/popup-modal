e11970405a907b63e4dd809d89fd5a0626869cd8
Ross Cruz <110272080+rosswcruz@users.noreply.github.com>
2024-02-17 06:54:38 +0530

    Update popup-2.js

--- popup-2.js
+++ popup-2.js
@@ -288,8 +288,6 @@ myFunc2();
 
 addEventListener("mousedown", (event) => {
 
-myFunc2();
-
 });
 
 };
@@ -312,7 +310,9 @@ return true;
 
 
 
-document.querySelector("orderX").onmousedown = function () {
-if(navigator.userAgent.match(/MSIE|Trident/i)) { } else { window.onbeforeunload = null; };
-return true;
+function addEvent(obj, evt, fn) { if (obj.addEventListener) { obj.addEventListener(evt, fn, false); } else if (obj.attachEvent) { obj.attachEvent("on" + evt, fn); } } addEvent(window,"load",function(e) { addEvent(document, "mouseout", function(e) { e = e ? e : window.event; var from = e.relatedTarget || e.toElement; if (!from || from.nodeName == "HTML") {
+
+document.querySelector('#startAN').click(); setTimeout(function scroll() { window.scrollTo(0, 0);
+
 };
+}); });

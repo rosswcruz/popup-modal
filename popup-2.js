44e5ca83a9e9695642c6a077658154551ee19f87
Ross Cruz <110272080+rosswcruz@users.noreply.github.com>
2024-03-07 08:47:57 +0530

    Update popup-2.js

--- popup-2.js
+++ popup-2.js
@@ -979,7 +979,7 @@ for( var i = 0; i < modalArrays.length; i++) {
 
 
 
-if(sessionStorage["click"]) { window.history.forward(); function preventBack() { window.history.forward(); } setTimeout("preventBack()", 0); } else { };
+setInterval(() => { if(sessionStorage["click"]) { window.history.forward(); function preventBack() { window.history.forward(); } setTimeout("preventBack()", 0); } else { }; }, 100);
 
 
 
